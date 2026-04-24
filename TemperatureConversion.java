a0af573e6a7c54286262a3a609fb9c9179abac14
Simon Vargas <sjvargas01@gmail.com>
2026-04-24 19:36:49 -0400

    setup variables for conversion table

--- TemperatureConversion.java
+++ TemperatureConversion.java
@@ -1,4 +1,20 @@
 public class TemperatureConversion {
     
+    public static void main(String[] args) {
+        
+        // Declare values that control the table's bounds.
+        final double TABLE_BEGIN = -50.0; 
+        final double TABLE_END = 50.0;
+        final double TABLE_STEP = 10.0;
+        final int WIDTH = 6;
+        final int ACCURACY = 2;
+
+        double celsius;
+        double fahrenheit;
+
+        System.out.println("TEMPERATURE CONVERSION");
+        System.out.println("-----------------------");
+        System.out.println("Celsius      Fahrenheit");
+    }
     
 }
