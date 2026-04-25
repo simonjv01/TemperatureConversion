011a2a75cb4555d5feed44e89b3d0f66480d115a
Simon Vargas <sjvargas01@gmail.com>
2026-04-24 20:29:34 -0400

    refactored to fix table output

--- TemperatureConversion.java
+++ TemperatureConversion.java
@@ -1,5 +1,3 @@
-import java.text.NumberFormat;
-
 public class TemperatureConversion {
     
     public static void main(String[] args) {
@@ -8,65 +6,28 @@ public class TemperatureConversion {
         final double TABLE_BEGIN = -50.0; 
         final double TABLE_END = 50.0;
         final double TABLE_STEP = 10.0;
-        final int WIDTH = 6;
         final int ACCURACY = 2;
 
-        double celsius;
-        double fahrenheit;
-
         System.out.println("TEMPERATURE CONVERSION");
-        System.out.println("-----------------------");
-        System.out.println("Celsius      Fahrenheit");
+        System.out.printf("%10s | %10s%n", "Celsius", "Fahrenheit");
+        System.out.println("-----------+------------");
 
-        for (celsius = TABLE_BEGIN; celsius <= TABLE_END; celsius += TABLE_STEP) {
-            // tHE for-loop has set celsius equal to the next celsius value of the table.
-            fahrenheit = celsiusToFahrenheit(celsius);
-            printNumber(celsius, WIDTH, ACCURACY);
-            System.out.println("C      ");
-            printNumber(fahrenheit, WIDTH, ACCURACY);
-            System.out.println("F");
-        }
-        System.out.println("-----------------------");
-        }
-
-    private static void printNumber(double d, int minWidth, int fractionDigits) {
-        // Note: getNumberInstance() creates a NumberFormat object using local 
-        // information about the characters for a decimal point and separators.
-        NumberFormat form = NumberFormat.getNumberInstance();
-        String output;
-        int i;
-        int length;
-
-        // Set the number of digits to appear on the right of the decimal.
-        if (fractionDigits < 0)
-            throw new IllegalArgumentException("fractionDigits < 0:" + fractionDigits);
-        
-        form.setMinimumFractionDigits(fractionDigits);
-        form.setMaximumFractionDigits(fractionDigits);
-
-        // Round and format the number. Added code to handle a Java bug that occurs when fractionDigits
-        // is zero.
-        output = form.format(d);
-        length = output.length();
-        if (fractionDigits == 0 && length > 1 && output.charAt(length-2) == '.')
-            output = " " + output.substring(0, length-2);
-        
-        // print any leading spaces and the number itself.
-        for (i = output.length(); i < minWidth; i++) {
-            System.out.println(' ');
-        System.out.println(output);
+        String rowFormat = "%10." + ACCURACY + "f | %10." + ACCURACY + "f%n";
+        for (double celsius = TABLE_BEGIN; celsius <= TABLE_END + 1e-9; celsius += TABLE_STEP) {
+            double fahrenheit = celsiusToFahrenheit(celsius);
+            System.out.printf(rowFormat, celsius, fahrenheit);
         }
     }
 
     private static double celsiusToFahrenheit(double c) {
-    
         final double MINIMUM_CELSIUS = -273.16;
-        if (c < MINIMUM_CELSIUS) 
+        if (c < MINIMUM_CELSIUS) {
             throw new IllegalArgumentException("Argument " + c + " is too low.");
-        return (9.0 / 5.0) * c + 32.0;
         }
-        
+        return (9.0 / 5.0) * c + 32.0;
     }
+
+}
 
     
 
